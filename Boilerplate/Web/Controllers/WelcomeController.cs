bbbca9dc603c302357da751a6e3339be19c15bde
hedgehog <hedgehog.follower@gmail.com>
2020-12-08 13:53:22 +0100

    added first initial controller to play with it

---
+++ Boilerplate/Web/Controllers/WelcomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Hedgehog", activity = "follower" });
+        }
+    }
+}
