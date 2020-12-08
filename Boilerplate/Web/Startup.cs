521850e2efe5f73dec215d2e949345ef5ea2818f
hedgehog <hedgehog.follower@gmail.com>
2020-12-08 13:52:37 +0100

    enable controllers mapping

--- Boilerplate/Web/Startup.cs
+++ Boilerplate/Web/Startup.cs
@@ -16,6 +16,8 @@ namespace Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,10 +28,17 @@ namespace Web
                 app.UseDeveloperExceptionPage();
             }
 
+            // Endpoint routing separates the process of selecting which "endpoint" will execute from the actual running of that endpoint.
+            // An endpoint consists of a path pattern, and something to execute when called.
+
+            // The UseRouting() extension method is what looks at the incoming request and decides which endpoint should execute.
+            // Any middleware that appears after the UseRouting() call will know which endpoint will run eventually.
             app.UseRouting();
 
+            // The UseEndpoints() call is responsible for configuring the endpoints, but also for executing them.
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
