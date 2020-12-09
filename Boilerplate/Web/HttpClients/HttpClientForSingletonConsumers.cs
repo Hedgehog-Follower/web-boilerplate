16b3579f92409a8de801b206c9a4cd4d5b24dcc8
hedgehog <hedgehog.follower@gmail.com>
2020-12-09 18:35:01 +0100

    added factory/wrapper to work with transient named httpclient under singleton consumer

---
+++ Boilerplate/Web/HttpClients/HttpClientForSingletonConsumers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Web.HttpClients
+{
+    public class HttpClientForSingletonConsumers : IHttpClientForSingletonConsumers
+    {
+        private readonly ServiceProvider _serviceProvider;
+
+        public HttpClientForSingletonConsumers(ServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public ITestClient GetTestClient()
+        {
+            return _serviceProvider.GetRequiredService<ITestClient>();
+        }
+    }
+
+    public interface IHttpClientForSingletonConsumers
+    {
+        ITestClient GetTestClient();
+    }
+}
--- Boilerplate/Web/Startup.cs
+++ Boilerplate/Web/Startup.cs
@@ -51,6 +51,8 @@ namespace Web
                 .AddHttpMessageHandler<ValidateHeaderHandler>()
                 .AddPolicyHandler(PollyPolicies.GetRetryPolicy())
                 .AddPolicyHandler(PollyPolicies.GetTimeoutPolicy());
+
+            services.AddSingleton<IHttpClientForSingletonConsumers, HttpClientForSingletonConsumers>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
